a859b4b0f4ca9891104ca309c85087f58640a5d9
maksim Kornakov <makskornakov@MacBook-Air-maksim.local>
2018-04-29 22:41:06 +0300

    hui

--- prj/includes/dph.inc.php
+++
@@ -1,5 +0,0 @@
- <?php
-$link = mysqli_connect('localhost', 'u160687_project', '128653', 'u160687_project');
-if (mysqli_connect_errno()) {
-	exit();
-}
--- prj/includes/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function get_games(){
-	global $link;
-	$sql = "SELECT * FROM games";
-	$result = mysqli_query($link, $sql);
-	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-	return $posts;
-}
